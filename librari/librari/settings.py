90496f7c4ced74dfe4a0e6126f215ba38a83d769
User <ushakov.ip@yandex.ru>
2023-02-12 20:21:42 +0300

    task_10

--- librari/librari/settings.py
+++ librari/librari/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'authors',
     'django_filters',
     'rest_framework.authtoken',
-    'drf_yasg'
+    'drf_yasg',
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
@@ -151,6 +152,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2
-
 }
 
+GRAPHENE = {
+    'SCHEMA': 'librari.schema.schema'
+}
